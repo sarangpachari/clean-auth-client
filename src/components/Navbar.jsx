34c0a00b701ea9e67bbf6d757bec845bffbe918a
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-04-21 00:29:42 +0530

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/clean-auth-logo.svg";
+import { MdLogout } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <header>
+        <nav className="w-full h-16 bg-blue-100 shadow-lg shadow-blue-100">
+          <div className="w-full h-full flex justify-evenly items-center">
+            {/* LEFT - LOGO */}
+            <div className="">
+              <img
+                src={logo}
+                className="w-12 rounded-full shadow-blue-300 shadow-sm"
+                alt="AUTH LOGO"
+              />
+            </div>
+            {/* CENTER - TABS */}
+            <div className="text-blue-600 font-semibold">
+              <ul className="flex justify-center items-center gap-4">
+                <li>Home</li>
+                <li>Features</li>
+                <li>About</li>
+              </ul>
+            </div>
+            {/* RIGHT - LOGOUT */}
+            <button className="border-l-4 border-blue-400 p-2 rounded-xl shadow-sm">
+              <MdLogout size={28} className="text-blue-500" />
+            </button>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import LoginForm from "../../components/auth/LoginForm";
+import RegisterForm from "../../components/auth/RegisterForm";
+import { useSearchParams } from "react-router-dom";
 
 const Auth = () => {
+  const [searchParams,setSearchParams] = useSearchParams();
+  const type  = searchParams.get("type") || "login" ;
+  const handleTabSwitch = (tab) => {
+    setSearchParams({type :tab})
+  };
   return (
-    <div>Auth</div>
-  )
-}
+    <>
+    <button onClick={()=>handleTabSwitch("login")}>SIGN IN</button>
+    <button onClick={()=>handleTabSwitch("register")}>SIGN UP</button>
+      {type === "login" ? <LoginForm /> : <RegisterForm />}
+    </>
+  );
+};
 
-export default Auth
+export default Auth;
