99bfbfea896b62343471b3cd95949938f8a99b2c
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-04-21 18:03:54 +0530

    designing completed

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { IoEyeOffOutline } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
+import { GoArrowRight } from "react-icons/go";
+import { motion } from "framer-motion";
+import { loadingContext } from "../../stores/AuthContext";
 
 const RegisterForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const [formData, setFormData] = useState({});
+  const { setLoading } = useContext(loadingContext);
+  const handlePasswordToggle = () => setShowPassword(!showPassword);
+  const handleLoading = () => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 2000);
+  };
+  const handleChange = (e, name) => {
+    setFormData({
+      ...formData,
+      [name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password || !formData.confirmPassword) {
+      setError("Please fill in all fields.");
+      return;
+    }
+    if (formData.password !== formData.confirmPassword) {
+      setError("Passwords do not match.");
+      return;
+    }
+    setError("");
+    console.log("Submitted");
+    handleLoading();
+    console.log(formData);
+  };
   return (
     <>
       <section>
@@ -14,22 +49,70 @@ const RegisterForm = () => {
               tasks. It's quick and easy!
             </p>
           </div>
-          <div className="flex gap-4 md:flex-row flex-col w-full">
-            <input
-              type="email"
-              placeholder="Email address"
-              className="max-w-72 md:p-4 p-2 border-s-2 rounded border-s-slate-500 focus:border-s-red-600 focus:outline-none focus:ring-0"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="max-w-72 md:p-4 p-2 border-s-2 rounded border-s-slate-500 focus:border-s-red-600 focus:outline-none focus:ring-0"
-            />
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              className="max-w-72 md:p-4 p-2 border-s-2 rounded border-s-slate-500 focus:border-s-red-600 focus:outline-none focus:ring-0"
-            />
+          <div className="md:w-2/3 flex flex-col gap-8">
+            <div className="flex gap-4 md:flex-row flex-col">
+              <input
+                type="email"
+                placeholder="Email address"
+                onChange={(e) => handleChange(e, "email")}
+                className="w-full bg-red-50 md:p-4 p-2 border-s-2 rounded border-s-slate-500 focus:border-s-red-600 focus:outline-none focus:ring-0"
+              />
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  onChange={(e) => handleChange(e, "password")}
+                  className="max-w-72 bg-red-50 md:p-4 p-2 border-s-2 rounded border-s-slate-500 focus:border-s-red-600 focus:outline-none focus:ring-0"
+                />
+                <div
+                  className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+                  onClick={handlePasswordToggle}
+                >
+                  {showPassword ? <IoEye /> : <IoEyeOffOutline />}
+                </div>
+              </div>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  onChange={(e) => handleChange(e, "confirmPassword")}
+                  className="max-w-72 bg-red-50 md:p-4 p-2 border-s-2 rounded border-s-slate-500 focus:border-s-red-600 focus:outline-none focus:ring-0"
+                />
+                <div
+                  className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+                  onClick={handlePasswordToggle}
+                >
+                  {showPassword ? <IoEye /> : <IoEyeOffOutline />}
+                </div>
+              </div>
+            </div>
+            {error && (
+              <div className="">
+                <span className="text-red-600 text-sm">{error}</span>
+              </div>
+            )}
+            <div className="flex w-full">
+              <motion.button
+                initial="initial"
+                whileHover="hover"
+                onClick={handleSubmit}
+                className="flex items-center border-b-2 text-red-700 font-medium p-2 rounded border-red-700 cursor-pointer"
+              >
+                <span>Go</span>
+                <motion.span
+                  variants={{
+                    initial: {
+                      marginLeft: 8,
+                    },
+                    hover: {
+                      marginLeft: 16,
+                    },
+                  }}
+                >
+                  <GoArrowRight size={24} />
+                </motion.span>
+              </motion.button>
+            </div>
           </div>
         </div>
       </section>
